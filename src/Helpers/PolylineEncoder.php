34c40c2c8b7a7f6981f00c50640139602939d16b
Henri de Jong <henridejong@gmail.com>
2018-07-30 22:05:38 +0100

    Add GPX download

---
+++ src/Helpers/PolylineEncoder.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * PHP Polyline Encoder
+ * From https://github.com/dyaaj/polyline-encoder
+ */
+
+class PolylineEncoder
+{
+
+    private $points;
+    private $encoded;
+
+    public function __construct()
+    {
+        $this->points = array();
+    }
+
+    /**
+     * Decode an encoded polyline string to an array of points
+     *
+     * @param string $value
+     * @return array
+     */
+    static public function decodeValue($value)
+    {
+        $index = 0;
+        $points = array();
+        $lat = 0;
+        $lng = 0;
+        while ($index < strlen($value)) {
+            $b;
+            $shift = 0;
+            $result = 0;
+            do {
+                $b = ord(substr($value, $index++, 1)) - 63;
+                $result |= ($b & 0x1f) << $shift;
+                $shift += 5;
+            } while ($b > 31);
+            $dlat = (($result & 1) ? ~($result >> 1) : ($result >> 1));
+            $lat += $dlat;
+            $shift = 0;
+            $result = 0;
+            do {
+                $b = ord(substr($value, $index++, 1)) - 63;
+                $result |= ($b & 0x1f) << $shift;
+                $shift += 5;
+            } while ($b > 31);
+            $dlng = (($result & 1) ? ~($result >> 1) : ($result >> 1));
+            $lng += $dlng;
+            $points[] = array('x' => $lat / 100000, 'y' => $lng / 100000);
+        }
+        return $points;
+    }
+
+    /**
+     * Add a point
+     *
+     * @param float $lat : latitude
+     * @param float $lng : longitude
+     */
+    public function addPoint($lat, $lng)
+    {
+        if (empty($this->points)) {
+            $this->points[] = array('x' => $lat, 'y' => $lng);
+            $this->encoded = $this->encodeValue($lat) . $this->encodeValue($lng);
+        } else {
+            $n = count($this->points);
+            $prev_p = $this->points[$n - 1];
+            $this->points[] = array('x' => $lat, 'y' => $lng);
+            $this->encoded .= $this->encodeValue($lat - $prev_p['x']) . $this->encodeValue($lng - $prev_p['y']);
+        }
+    }
+
+    /**
+     * Encode a value following Google Maps API v3 algorithm
+     *
+     * @param float $value
+     * @return string
+     */
+    protected function encodeValue($value)
+    {
+        $encoded = "";
+        $value = round($value * 100000);
+        $r = ($value < 0) ? ~($value << 1) : ($value << 1);
+
+        while ($r >= 0x20) {
+            $val = (0x20 | ($r & 0x1f)) + 63;
+            $encoded .= chr($val);
+            $r >>= 5;
+        }
+        $lastVal = $r + 63;
+        $encoded .= chr($lastVal);
+        return $encoded;
+    }
+
+    /**
+     * Return the encoded string generated from the points
+     *
+     * @return string
+     */
+    public function encodedString()
+    {
+        return $this->encoded;
+    }
+}
